e26d2b656736ebf6fd08de04773a56f8913123ec
Daniel Prado <danieloprado@outlook.com>
2015-10-22 13:37:07 +0100

    change modal template

--- src/bootstrap/services/modal.js
+++ src/bootstrap/services/modal.js
@@ -7,13 +7,13 @@
     function service($uibModal) {
         /*jshint validthis:true */
 
-        var template = '<div class="modal-header"><button type="button" class="close" ng-click="$dismiss()"> <span aria-hidden="true">&times;</span> </button> <h3 class="modal-title">{{title}}</h3> </div><div class="modal-body confirm-alert" ng-bind-html="content"></div><div class="modal-footer"> <div ng-show="!showCancel"> <button type="button" class="btn btn-success btn-addon" ng-click="$close()"> <i class="fa fa-check"></i>{{resource.ok}}</button> </div><div ng-show="showCancel"> <button type="button" class="btn btn-success btn-addon" ng-click="$close()"> <i class="fa fa-check"></i>{{resource.yes}}</button> <button type="button" class="btn btn-danger btn-addon" ng-click="$dismiss()"> <i class="fa fa-times"></i>{{resource.no}}</button> </div></div>';
+        var template = '<div class="modal-header"><button type="button" class="close" ng-click="$dismiss()"><span aria-hidden="true">&times;</span> </button> <h3 class="modal-title">{{title}}</h3> </div><div class="modal-body confirm-alert" ng-bind-html="content"></div><div class="modal-footer"> <div ng-show="!showCancel"> <button type="button" class="btn btn-success" ng-click="$close()">{{resource.ok}}</button> </div><div ng-show="showCancel"> <button type="button" class="btn btn-success" ng-click="$close()">{{resource.yes}}</button> <button type="button" class="btn btn-default" ng-click="$dismiss()">{{resource.no}}</button> </div></div>';
         var resource = {
             alertTitle: "Alert",
             confirmTitle: "Confirm",
             ok: "Ok",
-            yes: "Yes",
-            no: "no"
+            yes: "Ok",
+            no: "Cancel"
         };
 
         var openModal = function(title, content, showCancel) {
